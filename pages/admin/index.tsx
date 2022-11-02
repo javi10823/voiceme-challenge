e46b718542a62022dcfdc44d6cf05fefeeb3d7ae
Javier Olivieri <javierolivieri@Javiers-MacBook-Pro-2.local>
2022-11-01 21:07:59 -0300

    deleted mocked data on frontend

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -7,28 +7,18 @@ import { BASE_URL } from "../../config";
 const Admin = () => {
   const { isLoading, error, data } = useQuery({
     queryKey: ["adminData"],
-    queryFn: () => fetch(`${BASE_URL}/admin`).then((res) => res.json()),
+    queryFn: () => fetch(`${BASE_URL}/api/admin`).then((res) => res.json()),
   });
-
-  console.log({isLoading, error, data})
   const colors = ["#d72c4a", "#d75f2c", "#2cd79b", "#3d2cd7"];
-  const mockdata = [
-    { quarter: "a", earnings: 13 },
-    { quarter: "b", earnings: 16 },
-    { quarter: "c", earnings: 14 },
-    { quarter: "d", earnings: 19 },
-  ];
 
-  const dataPie = [
-    { x: "Cats", y: 35 },
-    { x: "Dogs", y: 40 },
-    { x: "Birds", y: 55 },
-  ];
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
-  const likes = 7;
-  const dislikes = 3;
+  if (error) {
+    return <h1>Error!</h1>;
+  }
 
-  const comics = ["comic1", "comic2", "comic3"];
   return (
     <div>
       <Head>
@@ -44,17 +34,17 @@ const Admin = () => {
             display: "flex",
             justifyContent: "space-between",
           }}>
-          {colors.map((i, ind) => (
+          {data.kpis?.map(({ title, value }, ind) => (
             <div
               key={ind}
               style={{
                 width: "20%",
                 paddingBlock: 10,
                 paddingInline: 20,
-                backgroundColor: i,
+                backgroundColor: colors[ind],
                 borderRadius: 10,
               }}>
-              <p style={{ fontWeight: 600 }}>TITLE title</p>
+              <p style={{ fontWeight: 600 }}>{title}</p>
               <div
                 style={{
                   display: "flex",
@@ -71,8 +61,7 @@ const Admin = () => {
                   }}
                 />
                 <div style={{ display: "flex" }}>
-                  <h1>19</h1>
-                  <h1>HS</h1>
+                  <h1>{value}</h1>
                 </div>
               </div>
             </div>
@@ -90,25 +79,35 @@ const Admin = () => {
               backgroundColor: "#822cd7",
               borderRadius: 20,
               paddingInline: 20,
-              width: "50%",
+              width: "45%",
             }}>
-            <h1>graph 1</h1>
-            <div style={{ width: "60%" }}>
+            <h1>Earnings</h1>
+            <div>
               <VictoryChart domainPadding={20}>
-                <VictoryBar data={mockdata} x="quarter" y="earnings" />
+                <VictoryBar
+                  data={data.earnings}
+                  x="quarter"
+                  y="earnings"
+                  style={{ data: { fill: "#FFF" } }}
+                />
               </VictoryChart>
             </div>
           </div>
           <div
             style={{
+              flexDirection: "column",
               backgroundColor: "#822cd7",
               borderRadius: 10,
               paddingInline: 20,
-              width: "45%",
+              width: "50%",
             }}>
-            <h1>graph 2</h1>
-            <div style={{ width: "60%" }}>
-              <VictoryPie colorScale={colors} data={dataPie} />
+            <h1>Popular comics</h1>
+            <div>
+              <VictoryPie
+                colorScale={colors}
+                data={data.popular}
+                style={{ labels: { fontSize: 16, fill: "#FFF" } }}
+              />
             </div>
           </div>
         </div>
@@ -123,26 +122,29 @@ const Admin = () => {
             borderRadius: 20,
             flexDirection: "column",
           }}>
-          {comics.map((i, ind) => (
+          <h1>Last comics rating</h1>
+          {data.lastComicsRating?.map(({ title, likes, dislikes }, ind) => (
             <div key={ind}>
-              <h2>{i}</h2>
+              <h2>{title}</h2>
               <div style={{ display: "flex", alignItems: "center" }}>
                 <div
                   style={{
                     backgroundColor: "gray",
-                    width: "50%",
+                    width: "100%",
                     height: 10,
                     marginRight: 20,
                   }}>
                   <div
                     style={{
                       height: 10,
-                      width: `${(likes * 100) / (likes + dislikes)}%`,
+                      width: `${Math.floor(
+                        (likes * 100) / (likes + dislikes)
+                      )}%`,
                       backgroundColor: "blue",
                     }}
                   />
                 </div>
-                <p>{(likes * 100) / (likes + dislikes)}%</p>
+                <p>{Math.floor((likes * 100) / (likes + dislikes))}%</p>
               </div>
             </div>
           ))}
--- pages/api/admin.ts
+++ pages/api/admin.ts
@@ -1,26 +1,32 @@
 const mock_data = {
-  visitors: [
-    {
-      time: Date.now() - 60 * 60 * 1000,
-      amount: 150,
-    },
-    {
-      time: Date.now() - 2 * 60 * 60 * 1000,
-      amount: 200,
-    },
-    {
-      time: Date.now() - 3 * 60 * 60 * 1000,
-      amount: 75,
-    },
-    {
-      time: Date.now() - 4 * 60 * 60 * 1000,
-      amount: 133,
-    },
+  earnings: [
+    { quarter: "1st", earnings: 13 },
+    { quarter: "2nd", earnings: 16 },
+    { quarter: "3rd", earnings: 14 },
+    { quarter: "4th", earnings: 19 },
   ],
+  kpis: [
+    { title: "Monthly earnings average", value: 15.5 },
+    { title: "Monthly visitors average", value: 534 },
+    { title: "Monthly earned coins", value: 789 },
+    { title: "Monthly coins spent", value: 246 },
+  ],
+  popular: [
+    { x: "Cats", y: 35 },
+    { x: "Dogs", y: 40 },
+    { x: "Birds", y: 55 },
+  ],
+  lastComicsRating: [
+    { title: 'Comic 1', likes: 53, dislikes: 23 },
+    { title: 'Comic 2', likes: 34, dislikes: 2 },
+    { title: 'Comic 3', likes: 25, dislikes: 64 },
+  ]
 };
 
 export default function getData(req, res) {
   if (req.method === "GET") {
-    res.status(200).json({ data: mock_data });
+    return res.status(200).json({ ...mock_data });
   }
-};
+
+  res.status(404).json({ error: "Unknown method" });
+}
