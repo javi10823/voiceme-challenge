7f8c1942616420b78212d05e3a0f460f90b39e9f
javi10823 <javi10823@gmail.com>
2022-11-02 10:54:52 -0300

    Minor fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import { VictoryBar, VictoryPie, VictoryChart, VictoryTheme } from "victory";
 import Link from "next/link";
 
 export default function Home() {
@@ -11,8 +9,17 @@ export default function Home() {
         <meta name="description" content="VoiceMe Admin" />
         <link rel="icon" href="/logo.svg" />
       </Head>
-      <Link href="/playground/coins">Coins playground</Link>
-      <Link href="/admin">Admin</Link>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Link href="/admin">Admin</Link>
+        <br />
+        <Link href="/playground/coins">Coins playground</Link>
+      </div>
     </div>
   );
 }
